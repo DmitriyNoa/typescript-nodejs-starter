0e0c41875038d91f59768388241c943b04f0649b
dzharkov <dmytro.zharkov@zalando.de>
2018-03-29 14:47:55 +0200

    Formatting

--- src/decorators/RouteValidator.ts
+++ src/decorators/RouteValidator.ts
@@ -2,35 +2,21 @@ export function Validate(params: Array<any>): any {
   return (target: Object, propertyKey: string): TypedPropertyDescriptor<any> => {
     const descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
     const originalMethod = descriptor.value;
-
     descriptor.value = function () {
       const errors: Array<any> = [];
-      const body = arguments[0];
-
+      const body = arguments[0].body;
       params.forEach((currentParam: any) => {
-        switch (currentParam.validate) {
-          case "email":
-            const check = new RegExp("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b", "gi");
-            if (!check.test(body[currentParam.param])) {
-              errors.push(currentParam);
-            }
-          break;
-          case "required":
-          default:
-            if (!body[currentParam.param]) {
-              errors.push(currentParam);
-            }
-          break;
+        if (!body[currentParam.param]) {
+          errors.push(currentParam);
         }
       });
 
       if (errors.length) {
-        return Promise.reject(errors);
+        return arguments[1].status(400).json({ errors: errors });
       } else {
         return originalMethod.apply(this, arguments);
       }
     };
-
     return descriptor;
   };
 }
