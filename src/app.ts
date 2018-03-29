c3612ebd543a2f1a08d0aa16948622d3b2cba4f6
dzharkov <dmytro.zharkov@zalando.de>
2018-03-29 17:35:51 +0200

    Add links to default route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import Server from "./classes/Server";
 import ArticlesRoute from "./routes/Shoes.route";
-
+import IndexRoute from "./routes/Index.route";
 const app = new Server(8080);
 
 const articles = new ArticlesRoute();
 app.addRoute("/articles", articles.router);
+
+const index = new IndexRoute(app.getRoutes());
+app.addRoute("/", index.router);
 
 app.start();
---
+++ src/routes/Index.route.ts
@@ -0,0 +1,25 @@
+import { Request, Response, Router } from "express";
+
+class IndexRoute {
+  public router: Router;
+
+  constructor(public applicationRoutes: string[]) {
+    this.router = Router();
+    this.init();
+  }
+
+  public init() {
+    this.router.route("").get((request: Request, response: Response) => {
+      const fullUrl = request.protocol + "://" + request.get("host");
+      const meta = this.applicationRoutes.map((resourse: string) => {
+        return {
+          rel: resourse.replace(/\//g, ''),
+          href: fullUrl + resourse
+        };
+      });
+      response.json({links: meta});
+    });
+  }
+}
+
+export default IndexRoute;
